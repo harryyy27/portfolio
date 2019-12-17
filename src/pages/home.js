3176c110ed40290b560fa93c9dcff3658f3d3390
harryyy27 <harryyy27@gmail.com>
2019-12-17 23:39:01 +0000

    adjustment

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,9 +3,14 @@ import {Wrapper, Heading, SubHeading} from '../components/styled'
 
 const Home =()=> {
         const sizeArr = [Math.ceil(50+Math.random()*20),Math.ceil(30+Math.random()*20),Math.ceil(20+Math.random()*10),Math.ceil(30+Math.random()*10),Math.ceil(25+Math.random()*10)]
-
+        
         const [position,setPosition] = useState('absolute')
-        const [size,handleSize]= useState(sizeArr)
+        if(window.innerWidth<700){
+            var [size,handleSize]= useState(sizeArr.map(el=>el/2))
+        }
+        else {
+            var [size,handleSize]=useState(sizeArr)
+        }
         useEffect(()=>{
                 // setPosition("absolute");
                 return ()=>{
@@ -14,7 +19,7 @@ const Home =()=> {
         },[])
         useEffect(()=>{
             const handleResize = ()=>{
-                if(window.innerWidth<615){
+                if(window.innerWidth<700){
                     handleSize(()=>{
                         const arr =sizeArr.map(el=>el/2)
                         console.log('1',arr)
